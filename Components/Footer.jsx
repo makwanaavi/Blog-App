dd24b6a23792d42db6346e5119c7966e246635df
prabhu-woyce <pparmar@woyce.io>
2025-07-28 15:51:20 +0530

    update

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,22 +1,24 @@
-import { assets } from '@/Assets/assets'
-import Image from 'next/image'
-import React from 'react'
+import { assets } from "@/Assets/assets";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
-      <Image src={assets.logo_light} alt='Logo' width={120} />
-      <p className='text-white'>
+    <div className="flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center">
+      <Link href="/">
+        <Image src={assets.logo_light} alt="Logo" width={120} />
+      </Link>
+      <p className="text-white">
         © 2025 Next Blog App. All rights reserved. | Made with ❤️ by Makwana Avi
       </p>
-      <div className='flex'>
-        <Image src={assets.facebook_icon} alt='' width={40}  />
-        <Image src={assets.twitter_icon} alt='' width={40}  />
-        <Image src={assets.googleplus_icon} alt='' width={40}  />
-
+      <div className="flex">
+        <Image src={assets.facebook_icon} alt="" width={40} />
+        <Image src={assets.twitter_icon} alt="" width={40} />
+        <Image src={assets.googleplus_icon} alt="" width={40} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
