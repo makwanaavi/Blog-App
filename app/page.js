34c08f2da2fae7fbacad3f74d7043f9ee31d9d62
prabhu-woyce <pparmar@woyce.io>
2025-07-28 12:57:16 +0530

    #update Blogitem and Bloglist com

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+"use client"
+
+import BlogItem from "@/Components/BlogItem";
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <BlogList />
     </>
   );
 }
