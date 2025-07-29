6262c6059c6768c8ed60e76efdea5f8e5d1dec3b
prabhu-woyce <pparmar@woyce.io>
2025-07-29 12:41:45 +0530

    #update

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -2,7 +2,6 @@ import ConnectDB from "@/lib/config/db";
 import BlogModel from "@/lib/models/BlogModel";
 const { NextResponse } = require("next/server");
 import { writeFile } from "fs/promises";
-import { title } from "process";
 
 const LoadDB = async () => {
     await ConnectDB();
@@ -11,12 +10,15 @@ const LoadDB = async () => {
 LoadDB()
 
 export async function GET(request) {
-    console.log("Fetching blog data...");
-
-    const blogs = await BlogModel.find({});
-
-
-    return NextResponse.json({blogs});
+    const blogId = request.nextUrl.searchParams.get("id");
+    if (blogId) {
+        const blog = await BlogModel.findById(blogId);
+        console.log(blog);
+        return NextResponse.json({ blog });
+    } else {
+        const blogs = await BlogModel.find({});
+        return NextResponse.json({ blogs });
+    }
 }
 
 export async function POST(request) {
